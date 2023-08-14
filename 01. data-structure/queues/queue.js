8bc9af9ed3846b4dd3e052d372303d303dcf7c99
dcortesnet <dcortes.net@gmail.com>
2023-08-14 10:07:08 -0400

    Queue example

---
+++ 01. data-structure/queues/queue.js
@@ -0,0 +1,17 @@
+class Queue {
+  constructor() {
+    this.items = [];
+  }
+
+  enqueue(element) {
+    this.items.push(element);
+  }
+
+  dequeue() {
+    return this.items.shift();
+  }
+
+  peek() {
+    return this.items[0];
+  }
+}
